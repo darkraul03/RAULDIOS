e1d35eb6728ccca058031569c3dee5bf9cf2d152
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-10-29 19:46:42 -0500

    Inicio de Proyecto

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php $fecha = date('Y')?>
+<!Doctype html>
+<html>
+<head>
+	<title>FINNDER</title>
+	<link rel="stylesheet" href="estilos/estilo.css" />
+</head>
+<body>
+	<section id="contenedor">
+		<header>
+			<aside id="titulo">
+				<img src="logo/F_logo.png" height="250px" />
+			</aside>
+			<aside id="iniciar">
+				<a class="link"><span>Iniciar Sesion</span></a>
+				<a class="link"><span>Idioma</span></a>
+			</aside>
+		</header>
+		<aside>
+			<p><img src=""/>Profesores</p>
+			<p><img src=""/>Chat</p>
+			<p><img src=""/>Destacados</p>
+			<p><img src=""/>Perfil</p>
+			<p><img src=""/>Favoritos</p>
+			<p><img src=""/>Ajustes</p>
+		</aside>
+		<section>
+			
+		</section>
+	</section>
+	<footer>
+		<section>
+			<span>Contactanos</span>
+			<span>Soporte</span>
+			<span>Ayuda</span>
+			<span>Politica de Privacidad</span>
+			<span>Terminos de Uso</span>
+		</section>
+		<section>
+			<img src=""/>
+			<img src=""/>
+			<img src=""/>
+			<img src=""/>
+		</section>
+		<p>&copy; <? echo $fecha ?></p>
+	</footer>
+</body>
+</html>
