df9bd5a843fb930df50728703d850b31d605b993
spkv <spkv123@gmail.com>
2016-10-29 22:55:13 -0500

    mozzarella

--- index.php
+++ index.php
@@ -29,25 +29,7 @@
 		</section>
 	</section>
 	<footer>
-		<section id="servicios">
-			<span>Contactanos</span>
-			<br/>
-			<span>Soporte</span>
-			<br/>
-			<span>Ayuda</span>
-			<br/>
-			<span>Politica de Privacidad</span>
-			<br/>
-			<span>Terminos de Uso</span>
-			<br/>
-		</section>
-		<section id="social">
-			<a href="http://www.facebook.com"><img src="img/fbook_little.jpg"/></a>
-			<a href="http://www.youtube.com"><img src="img/youtube_little_pag.jpg"/></a>
-			<a href="http://www.twitter.com"><img src="img/twitter_little_pag.jpg"/></a>
-			<a href="http://www.linkdin.com"><img src="img/linkdin_little_pag.jpg"/></a>
-		</section>
-		<p id="copyright">&copy Nexus <?php echo $fecha?></p>
+		<?php include 'pie.php' ?>	
 	</footer>
 </body>
 </html>
