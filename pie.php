eefdb3c9d438045b90283c942d2cbac307b9d700
spkv <spkv123@gmail.com>
2016-10-29 22:59:00 -0500

    fecha 2.0

--- pie.php
+++ pie.php
@@ -1,4 +1,3 @@
-	<?php $fec = date('Y') ?>
 	<footer>
 		<section id="servicios">
 			<span>Contactanos</span>
@@ -13,10 +12,10 @@
 			<br/>
 		</section>
 		<section id="social">
-			<a href="http://www.facebook.com"><img src="img/fbook_little.jpg"/></a>
-			<a href="http://www.youtube.com"><img src="img/youtube_little_pag.jpg"/></a>
-			<a href="http://www.twitter.com"><img src="img/twitter_little_pag.jpg"/></a>
-			<a href="http://www.linkdin.com"><img src="img/linkdin_little_pag.jpg"/></a>
+			<a href="http://www.facebook.com"><img class="redes" src="img/fbook_little.jpg"/></a>
+			<a href="http://www.youtube.com"><img class="redes" src="img/youtube_little_pag.jpg"/></a>
+			<a href="http://www.twitter.com"><img class="redes" src="img/twitter_little_pag.jpg"/></a>
+			<a href="http://www.linkdin.com"><img class="redes" src="img/linkdin_little_pag.jpg"/></a>
 		</section>
-		<p id="copyright">&copy Nexus <?php echo $fec ?></p>
+		<p id="copyright">&copy Nexus <?php echo date("Y") ?></p>
 	</footer>
