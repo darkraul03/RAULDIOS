87424ec53dcf631b3f8350668056cbca765fcf82
spkv <spkv123@gmail.com>
2016-10-30 04:38:24 -0500

    Pizza???

--- indexSec.php
+++ indexSec.php
@@ -8,7 +8,7 @@
 	<section id="contenedor">
 		<header>
 			<aside id="iniciar">
-				<a class="link" href="inicio_sesion.php"><span>Iniciar Sesion</span></a>
+				<a class="link" href="inicio_sesion.php"><span>Iniciar Sesión</span></a>
 				<a class="link" href="registrarse.php"><span>Registrarme</span></a>
 			</aside>
 			<aside id="titulo">
@@ -16,18 +16,30 @@
 			</aside>
 			<br/>
 		</header>
-		<aside id="menu">
-			<a class="link" href="buscar_profesor.php"><img src="img/profesor_little.png"/><span>Profesores</span></a>
+
+		<div id="menu" class="sidenav">
+  			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>			
+			<a href="buscar_profesor.php"><img src="img/profesor_little.png"/><span>Profesores</span></a>
 			<br/><br/>
-			<a class="link" href="#"><img src="img/star.png"/><span>Destacados</span></a>
+			<a href="#"><img src="img/star.png"/><span>Destacados</span></a>
 			<br/><br/>
-			<a class="link" href="#"><img src="img/perfil.png"/><span>Perfil</span></a>
+			<a href="#"><img src="img/perfil.png"/><span>Perfil</span></a>
 			<br/><br/>
-			<a class="link" href="#"><img src="img/corazon.png"/><span>Favoritos</span></a>
+			<a href="#"><img src="img/corazon.png"/><span>Favoritos</span></a>
 			<br/><br/>
-			<a class="link" href="#"><img src="img/ajustes.png"/><span>Ajustes</span></a>
+			<a href="#"><img src="img/ajustes.png"/><span>Ajustes</span></a>
 			<br/><br/>
-		</aside>
+		</div>
+		<span id="abrir" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; menú</span>
+		<script>
+			function openNav() {
+			    document.getElementById("menu").style.width = "20%";
+			}
+
+			function closeNav() {
+			    document.getElementById("menu").style.width = "0";
+			}
+		</script>
 		<section>
 			
 		</section>
