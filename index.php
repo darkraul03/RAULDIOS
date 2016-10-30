70d81acbcfc844e6362e1f810bf7323004ccf0e1
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-10-29 22:33:16 -0500

    imagenes Menu

--- index.php
+++ index.php
@@ -12,16 +12,17 @@
 				<img id="logo" src="logo/F_logo.png" height="250px" />
 			</aside>
 			<aside id="iniciar">
+			    //
 				<a class="link" href="inicio_sesion.php"><span>Iniciar Sesion</span></a>
 				<a class="link" href="registrarse.php"><span>Registrarme</span></a>
 			</aside>
 		</header>
 		<aside>
-			<p><img src=""/>Profesores</p>
-			<p><img src=""/>Destacados</p>
-			<p><img src=""/>Perfil</p>
-			<p><img src=""/>Favoritos</p>
-			<p><img src=""/>Ajustes</p>
+			<a href="buscar_profesor.php"><p><img src="img/profesor_little.png"/>Profesores</p></a>
+			<a><p><img src="img/star.png"/>Destacados</p></a>
+			<a><p><img src="img/perfil.png"/>Perfil</p></a>
+			<a><p><img src=""/>Favoritos</p></a>
+			<a><p><img src=""/>Ajustes</p></a>
 		</aside>
 		<section>
 			
