dd64a6bfa3e98ed3436289dc8fbddd2f9433349e
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-10-30 02:34:12 -0500

    INDEXSEC.php
    para que vean lo que hice :v

---
+++ indexSec.php
@@ -0,0 +1,39 @@
+<!Doctype html>
+<html>
+<head>
+	<title>FINNDER</title>
+	<link rel="stylesheet" href="estilos2/estilo.css" type="text/css" media="all" />
+</head>
+<body>
+	<section id="contenedor">
+		<header>
+			<aside id="iniciar">
+				<a class="link" href="inicio_sesion.php"><span>Iniciar Sesion</span></a>
+				<a class="link" href="registrarse.php"><span>Registrarme</span></a>
+			</aside>
+			<aside id="titulo">
+				<img id="logo" src="logo/F_logo_w.png" height="180px" />
+			</aside>
+			<br/>
+		</header>
+		<aside id="menu">
+			<a class="link" href="buscar_profesor.php"><img src="img/profesor_little.png"/><span>Profesores</span></a>
+			<br/><br/>
+			<a class="link" href="#"><img src="img/star.png"/><span>Destacados</span></a>
+			<br/><br/>
+			<a class="link" href="#"><img src="img/perfil.png"/><span>Perfil</span></a>
+			<br/><br/>
+			<a class="link" href="#"><img src="img/corazon.png"/><span>Favoritos</span></a>
+			<br/><br/>
+			<a class="link" href="#"><img src="img/ajustes.png"/><span>Ajustes</span></a>
+			<br/><br/>
+		</aside>
+		<section>
+			
+		</section>
+	</section>
+	<footer>
+		<?php include 'pie2.php' ?>	
+	</footer>
+</body>
+</html>
---
+++ pie2.php
@@ -0,0 +1,20 @@
+	<footer>
+		<section id="servicios">
+			<span><a class="link" href="#">Contactanos</a></span>
+			<span> | </span>
+			<span><a class="link" href="#">Soporte</a></span>
+			<span> | </span>
+			<span><a class="link" href="#">Ayuda</a></span>
+			<span> | </span>
+			<span><a class="link" href="#">Politica de Privacidad</a></span>
+			<span> | </span>
+			<span><a class="link" href="#">Terminos de Uso</a></span>
+		</section>
+		<section id="social">
+			<a href="http://www.facebook.com"><img class="redes" src="img/fbook_little.jpg"/></a>
+			<a href="http://www.youtube.com"><img class="redes" src="img/youtube_little_pag.jpg"/></a>
+			<a href="http://www.twitter.com"><img class="redes" src="img/twitter_little_pag.jpg"/></a>
+			<a href="http://www.linkdin.com"><img class="redes" src="img/linkdin_little_pag.jpg"/></a>
+		</section>
+		<span id="copyright">&copy Nexus <?php echo date("Y") ?></span>
+	</footer>
