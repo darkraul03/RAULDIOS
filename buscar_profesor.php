6580396a9fdaa551cdd6d12cab5b2dac8997e72e
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-11-06 16:46:42 -0500

    pagina de busqueda

--- buscar_profesor.php
+++ buscar_profesor.php
@@ -9,7 +9,7 @@
 	<?php include 'cabecera.php' ?>
 	<section>
 		<h1> Buscar Profesor</h1>
-		<form>
+		<form action="procesar_busqueda.php" method="post">
 			<label>Nombre</label>
 			<input type="text" name="nombre"/>
 			<br/><br/>
@@ -18,15 +18,26 @@
 			<br/><br/>
 			<label> Universidad </label>
 			<select name="universidad">
-				<option> Universidad San Ignacio de Loyola </option>
-				<option> Pontificie Universidad Catolica del Peru </option>
-				<option> Universidad de Lima </option>
-				<option> Universidad Pacifico </option>
-				<option> Universidad Cesar Vallejo</option>
+				<option value="Universidad San Ignacio de Loyola"> Universidad San Ignacio de Loyola </option>
+				<option value="Pontificie Universidad Catolica del Peru"> Pontificie Universidad Catolica del Peru </option>
+				<option value="Universidad de Lima"> Universidad de Lima </option>
+				<option value="Universidad Pacifico"> Universidad Pacifico </option>
+				<option value="Universidad Cesar Vallejo"> Universidad Cesar Vallejo</option>
 			</select>
 			<br/><br/>
 			<label> Curso  </label>
-			<input type="text" name="curso"/>
+			<select name="curso">
+				<option value="Calculo I"> Calculo I </option>
+				<option value="Calculo II"> Calculo II </option>
+				<option value="College Algebra"> College Algebra </option>
+				<option value="Filosofia Social"> Filosofia Social </option>
+				<option value="Fundamentos de los Sistemas de Informacion"> Fundamentos de los Sistemas de Informacion</option>
+				<option value="Programacion Orientada a Objetos"> Programacion Orientada a Objetos</option>
+				<option value="Quimica Pura"> Quimica Pura </option>
+				<option value="Seminario de Tesis"> Seminario de Tesis </option>
+				<option value="Taller de Software II"> Taller de Software II </option>
+				<option value="Total Wellness"> Total Wellness </option>
+			</select>
 			<br/><br/>
 			<input type="submit" value= "Buscar Profesor"/>			
 		</form>
