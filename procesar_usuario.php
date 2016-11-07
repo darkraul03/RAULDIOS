a949575dfbbf73642044d2890f4f0e69e2e8746c
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-11-06 19:48:53 -0500

    la pta comilla!! D:<

--- procesar_usuario.php
+++ procesar_usuario.php
@@ -1,24 +1,24 @@
 <?php
 
-	$usuario=$_POST['usuario'];
-	$nombres=$_POST['nombres'];
-	$apellidos=$_POST['apellidos'];
-	$correo=$_POST['correo'];
-	$uni=$_POST['universidad'];
-	$pass=$_POST['pass'];
-	$pass2=$_POST['pass2'];
+	$usuario=$_POST["usuario"];
+	$nombres=$_POST["nombres"];
+	$apellidos=$_POST["apellidos"];
+	$correo=$_POST["correo"];
+	$uni=$_POST["universidad"];
+	$pass=$_POST["pass"];
 
-	if($pass != $pass2){
-		header('Location:registro_usuario.php');
-	}else{
-		$password=md5($pass);
+	$password = md5($pass);
+
 		mysql_connect('localhost','root','') or die(mysql_error());
+
 		mysql_select_db('finnder') or die(mysql_error());
-		$query = "insert into usuarios(Correo,Usuario,Password,Nombre,Apellido,Universidad) values('$correo','$usuario','$password',$nombres','$pass','$apellidos', '$uni')";
-		mysql_query($query);
+
+		$resultado = mysql_query("SELECT * FROM usuarios") or die(mysql_error());
+
+		$sql = mysql_query("INSERT INTO usuarios(Correo, Usuario, Password, Nombre, Apellido, Universidad)  VALUES ('$correo','$usuario','$password','$nombres','$apellidos','$uni')") or die(mysql_error());
 		
 		header('Location:resultado_usuario.php');
-	}
+
 	
 	
 ?>
