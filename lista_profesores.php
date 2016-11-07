08c374645be7a70f5c9163734b6ded4044fca1c5
spkv <spkv123@gmail.com>
2016-11-06 20:52:33 -0500

    Add files via upload

--- lista_profesores.php
+++ lista_profesores.php
@@ -33,7 +33,21 @@
 				# Paso 4: iterar
 				while ($fila = mysql_fetch_assoc($request)) {
 				?>
-					<div id="lista">				
+					<div id="lista">
+						<form id="formestrella">
+							<p class="clasificacion">
+						    	<input class="estrella" id="radio1" type="radio" name="estrellas<?php echo $fila['codigo'] ?>" value="5">
+						    	<label class="calificar" for="radio1">★</label>
+						    	<input class="estrella" id="radio2" type="radio" name="estrellas<?php echo $fila['codigo'] ?>" value="4">
+						    	<label class="calificar" for="radio2">★</label>
+						    	<input class="estrella" id="radio3" type="radio" name="estrellas<?php echo $fila['codigo'] ?>" value="3">
+						    	<label class="calificar" for="radio3">★</label>
+						    	<input class="estrella" id="radio4" type="radio" name="estrellas<?php echo $fila['codigo'] ?>" value="2">
+						    	<label class="calificar" for="radio4">★</label>
+						    	<input class="estrella" id="radio5" type="radio" name="estrellas<?php echo $fila['codigo'] ?>" value="1">
+						    	<label class="calificar" for="radio5">★</label>
+						  	</p>
+						</form>				
 						<img src="<?php echo $fila['foto'] ?>" width ="200"/>
 						<span>Nombre:</span>
 						<p><?php echo utf8_encode($fila['Nombre']) ?> <?php echo utf8_encode($fila['Apellido']) ?></p>
