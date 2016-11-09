7de22916418b20fbc46b5b972098c41f52782203
potatoboy1999 <alejandrodazaculqui@hotmail.com>
2016-11-09 10:20:57 -0500

    DESCRIPCIÓN DEL INDEX

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<title>FINNDER</title>
 	<link rel="stylesheet" href="estilos/estilo.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="estilos/estilo2.css" type="text/css" media="all" />
 	<style>
 		header {
 			background-color: rgba(0, 0, 0,0.3);
@@ -16,7 +17,7 @@
 
 		<div id="menu" class="sidenav">
   			<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>			
-			<a href="buscar_profesor.php"><img src="img/Contact_book_icon_w.png" width="30px" /><span>Profesores</span></a>
+			<a href="lista_profesores.php"><img src="img/Contact_book_icon_w.png" width="30px" /><span>Profesores</span></a>
 			<br/><br/>
 			<a href="#"><img src="img/Star_icon_w.png" width="30px" /><span>Destacados</span></a>
 			<br/><br/>
@@ -28,7 +29,42 @@
 			<br/><br/>
 		</div>
 		<span id="abrir" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; menú</span>
-
+		<section id="descripcion">
+			<section id="bienvenida">
+				<div id="img_B">
+					<img src="#">
+				</div>
+				<div id="left_B">
+					<h2>BIENVENIDO A FINNDER</h2>
+					<p>
+						Finnder es una pagina web dedicado especialmente a los alumnos universitarios. Por Alumnos para alumnos.
+					</p>
+					<p>
+						Finnder es la ayuda que buscabas al momento de elegir con que profesor matricularte para tu nuevo periodo universitario.
+					</p>
+					<p>
+						Ofrecemos una variada lista de profesores dentro de nuestra base de datos para que, probablemente, puedas encontrar a los profesores del curso que buscas de tu universidad.
+					</p>
+				</div>
+			</section>
+			<section id="innnovar">
+				
+				<div id="right_I">
+					<h2>CREADO PARA INNOVAR</h2>
+					<p>
+						Finnder fue creado por alumnos de la Universidad San Ignacion de Loyola
+					</p>
+					<p>
+						Entendemos las necesitades de los alumnos y apuntamos a poder satisfacer todas estas
+					</p>
+				</div>
+				<div id="img_I">
+					<img src="#">
+				</div>
+			</section>
+			<section id="foot"></section>
+			
+		</section>
 	</section>
 
 	<footer>
